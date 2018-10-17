590106d7f210d3d4282f45c5a38d669f4a084309
spiperac <spiperac@denkei.org>
2018-10-17 02:00:17 +0200

    login added

--- bctf/urls.py
+++ bctf/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # apps
     path('', include('apps.scoreboard.urls')),
     path('challenges/', include('apps.challenges.urls')),
 
