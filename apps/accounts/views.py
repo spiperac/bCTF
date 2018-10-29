b93121297e50be5b2d5cf6ae53e6567fdaa15b6d
spiperac <spiperac@denkei.org>
2018-10-29 15:14:23 +0100

    basic profile integration;

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from apps.accounts.models import Account
 from apps.accounts.forms import AccountCreationForm
+from apps.challenges.models import Solves, FirstBlood
+
 
 class RegistrationView(CreateView):
     form_class = AccountCreationForm
@@ -14,3 +16,14 @@ class RegistrationView(CreateView):
         if request.user.is_authenticated:
             return HttpResponseRedirect(reverse_lazy('scoreboard:home'))
         return super(RegistrationView, self).dispatch(request, *args, **kwargs)
+
+
+class ProfileView(DetailView):
+    model = Account
+    template_name = 'account/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['solved'] = Solves.objects.filter(account=self.request.user.pk)
+        context['first_bloods'] = FirstBlood.objects.filter(account=self.request.user.pk)
+        return context
--- bctf/urls.py
+++ bctf/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from apps.accounts.views import RegistrationView
+from apps.accounts.views import RegistrationView, ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +10,7 @@ urlpatterns = [
     # accounts
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html', redirect_authenticated_user=True), name="login"),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
-
+    path('accounts/profile/<int:pk>', ProfileView.as_view(), name="profile"),
     path('registration/', RegistrationView.as_view(), name='registration'),
 
     # apps
