15ec7feea8f7f804262d0637e1422821a54b539a
spiperac <spiperac@denkei.org>
2018-10-31 22:10:00 +0100

    mostly cosmetic changes on docker and menu selector;

--- bctf/settings/base.py
+++ bctf/settings/base.py
@@ -1,9 +1,15 @@
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+# Load configuration
+config_file_path = os.path.join(BASE_DIR, '../config/config.json')
+with open(config_file_path) as f:
+    config_file = json.load(f)
 
 # Application definition
 
