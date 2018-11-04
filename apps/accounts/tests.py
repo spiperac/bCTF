7255e778586cbf9d4bd0246cd3888e5d58b2bbf9
badarg <spiperac@denkei.org>
2018-11-04 14:15:37 +0100

    basic account tests;

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from apps.accounts.models import Account
 
-# Create your tests here.
+
+class AccountTests(TestCase):
+    def setUp(self):
+        self.test_username = "tester"
+        self.test_password = "hacker312"
+        self.test_email = "tester@testland.com"
+
+    def test_registration(self):
+        client = Client()
+        request_data = {
+            "username": self.test_username,
+            "email": self.test_email,
+            "password1": self.test_password,
+            "password2": self.test_password
+        }
+
+        response = client.post(reverse('registration'), request_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Account.objects.filter(username=self.test_username).count() == 1)
+
+
+    def test_login(self):
+        client = Client()
+        request_data = {
+            "username": self.test_username,
+            "password": self.test_password
+        }
+        account = Account.objects.create(
+            username=self.test_username,
+            email=self.test_email
+        )
+
+        account.set_password(self.test_password)
+        account.save()
+
+        response = client.post(reverse('login'), request_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(int(client.session['_auth_user_id']), account.pk)
