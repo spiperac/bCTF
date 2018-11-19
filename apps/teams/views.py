88390363656a922114402a30f343069a0c844689
badarg <spiperac@denkei.org>
2018-11-19 13:33:10 +0100

    added teams;

---
+++ apps/teams/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+from apps.teams.models import Team
+
+
+class CreateTeamView(CreateView):
+    model = Team
+    template_name = 'team/create.html'
+
+
+class UpdateTeamView(UpdateView):
+    model = Team
+    template_name = 'team/update.html'
+
+
+class DetailTeamView(DetailView):
+    model = Team
+    template_name = 'team/detail.html'
+
+
+class ListTeamView(ListView):
+    model = Team
+    template_name = 'team/list.html'
+
+
+class DeleteTeamView(DeleteView):
+    model = Team
+    template_name = 'team/list.html'
