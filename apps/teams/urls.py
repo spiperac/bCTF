67671291c84d70e0b0d9884cc99994c99cfd99fb
spiperac <spiperac@denkei.org>
2018-11-19 14:34:38 +0100

    teams app staled as a feature integration;

---
+++ apps/teams/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from apps.teams.views import (CreateTeamView, UpdateTeamView, DetailTeamView,
+                                        ListTeamView, DeleteTeamView)
+
+app_name = 'teams'
+urlpatterns = [
+    path('', ListTeamView.as_view(), name='list'),
+    path('new/', CreateTeamView.as_view(), name='create'),
+    path('<int:pk>/', DetailTeamView.as_view(), name='detail'),
+    path('<int:pk>/update', UpdateTeamView.as_view(), name='update'),
+    path('<int:pk>/delete', DeleteTeamView.as_view(), name='delete'),
+]
