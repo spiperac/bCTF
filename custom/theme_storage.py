b7a01fbb91dc2130b0d771fc2e71e1623e2e9cf4
spiperac <spiperac@denkei.org>
2018-11-22 05:17:22 +0100

    fix?;

---
+++ custom/theme_storage.py
@@ -0,0 +1,16 @@
+from django.core.files.storage import FileSystemStorage
+from django.utils.encoding import filepath_to_uri
+from urllib.parse import urljoin
+
+from apps.scoreboard.utils import get_theme
+
+
+class ThemeStorage(FileSystemStorage):
+
+    def url(self, name):
+        if self.base_url is None:
+            raise ValueError("This file is not accessible via a URL.")
+        url = filepath_to_uri("{0}/static/{1}".format(get_theme(), name))
+        if url is not None:
+            url = url.lstrip('/')
+        return urljoin(self.base_url, url)
