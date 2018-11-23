d7a572a17b4fa366dab8e42d5c8c7dcd1b04b40c
spiperac <spiperac@denkei.org>
2018-11-23 20:19:03 +0100

    removed prefetch from ctftime plugin to make it faster; i will use one more view for maximum scoreboard;

--- plugins/ctftime/views.py
+++ plugins/ctftime/views.py
@@ -10,13 +10,14 @@ class CTFTimeView(View):
 		response = {}
 		response['tasks'] = [x.name for x in Challenge.objects.all()]
 		response['standings'] = []
-		accounts_scored = Account.objects.prefetch_related('solves').filter(points__gt=0).filter(is_active=True).order_by('-points')
+		accounts_scored = Account.objects.filter(points__gt=0).filter(is_active=True).order_by('-points')
 
 		for (rank, account) in enumerate(accounts_scored, start=1):
 			team = {}
 			team['pos'] = rank
 			team['team'] = account.username
 			team['score'] = account.points
+
 			response['standings'].append(team)
 
 		return JsonResponse(response)
