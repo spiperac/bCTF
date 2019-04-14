b201a76453efa06c1b41e57e4c7a5ea395b10eb5
spiperac <spiperac@denkei.org>
2019-04-14 14:07:00 +0200

    Missing cache on scoreboard page.

--- bctf/themes/core/static/js/index.js
+++ bctf/themes/core/static/js/index.js
@@ -23,7 +23,7 @@ function feed_events(element) {
         <li class="collection-item avatar">
         <i class="material-icons circle red">place</i>
         <span class="title">[${element.time}]</span>
-        <p><a href="/accounts/profile/${element.team_id}">${element.team}</a> just captured <a href="/challenges/#${element.challenge_id}" target="_blank">${element.challenge}</a>!</p>
+        <p><a href="/accounts/profile/${element.team_id}">${element.team}</a>  just captured  <a href="/challenges/#${element.challenge_id}" target="_blank">${element.challenge}</a>!</p>
         </li>
         `
     )
